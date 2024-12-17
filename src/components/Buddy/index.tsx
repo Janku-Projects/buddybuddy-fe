952acd6e8af4e5cb2e8b41c8905075a0d17c3412
heath <jannhyunsoo@gmail.com>
2024-12-18 08:08:15 +0900

    feature: canvas-setup

--- src/components/Buddy/index.tsx
+++ src/components/Buddy/index.tsx
@@ -1,17 +1,25 @@
 import { FC } from "react";
 import { BuddyWrapper } from "@/components/Buddy/Buddy.styled";
+import { Canvas } from "@react-three/fiber";
+
+import LightController from "@/components/Model/LightController";
+import OrbitController from "@/components/Model/OrbitController";
 
 interface iBuddy {
 
 }
 
-const Buddy: FC<iBuddy> = ({}) => {
+const Buddy: FC<any> = ({ fileName, isComponent = false, isShowLabel = true, level = 1, name = "" }) => {
 
 
     return (
         <BuddyWrapper>
-			Hello World
-		</BuddyWrapper>
+            <Canvas camera={{ position: [0, 0, 3], fov: isComponent ? 15 : 30 }}>
+
+                <LightController/>
+                <OrbitController/>
+            </Canvas>
+        </BuddyWrapper>
     );
 };
 
