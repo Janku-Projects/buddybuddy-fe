97a5864d75232d14a80710fe0aa6e0acdac6c9b2
heath <jannhyunsoo@gmail.com>
2024-12-27 23:50:43 +0900

    feature: icons-update

--- src/components/ActionNav/index.tsx
+++ src/components/ActionNav/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Container } from "@/components/ActionNav/ActionNav.styled";
 import { generalInterface } from "@/Util/util";
 import { FC } from "react";
+import Icon from "@/components/common/Icon";
 
 
 interface iActionNavProps extends generalInterface {
@@ -12,22 +13,19 @@ export const ActionNav: FC<iActionNavProps> = ({}) => {
     return (
         <Container>
             <Box onClick={() => {}}>
-                {/*<Icon width={30} height={30} icon="LiftingWeights" />*/}
+                <Icon width={30} height={30} icon="LiftingWeights" />
                 운동
             </Box>
             <Box onClick={() => {}}>
-            {/*<Box onClick={() => handleAction("SHOWER")}>*/}
-                {/*<Icon width={30} height={30} icon="Bathtub" />*/}
+                <Icon width={30} height={30} icon="Bathtub" />
                 샤워
             </Box>
             <Box onClick={() => {}}>
-            {/*<Box onClick={() => handleAction("SLEEP")}>*/}
-            {/*    <Icon width={30} height={30} icon="Bad" />*/}
+                        <Icon width={30} height={30} icon="Bad" />
                 잠자기
             </Box>
             <Box onClick={() => {}}>
-            {/*<Box onClick={handleTalkAction}>*/}
-                {/*<Icon width={30} height={30} icon="ThoughtBalloon" />*/}
+                <Icon width={30} height={30} icon="ThoughtBalloon" />
                 대화
             </Box>
         </Container>
---
+++ src/components/common/Icon/index.tsx
@@ -0,0 +1,23 @@
+import { FC, SVGAttributes } from "react";
+import * as icons from "@/components/common/Icon/Icons";
+
+interface iIconProps extends SVGAttributes<SVGElement> {
+    icon: keyof typeof icons;
+    width: number;
+    height?: number;
+    fill?: string;
+}
+
+const Icon: FC<iIconProps> = ({ icon, width = 24, height = 24, fill = "none", ...props }) => {
+    const SVGIcon = icons[icon]; // Resolve the icon dynamically
+
+    if (!SVGIcon) {
+        console.error(`Icon "${icon}" does not exist in the icons object.`);
+        return (<></>);
+    }
+    // @ts-ignore
+    return <SVGIcon width={width} height={height} fill={fill} {...props} />;
+};
+
+
+export default Icon;
