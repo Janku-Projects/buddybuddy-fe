65233cc57afab2fe8901bf81a091b37a877a2919
heath <jannhyunsoo@gmail.com>
2024-12-29 00:46:21 +0900

    test: icon-page

--- src/components/Test/icon.tsx
+++ src/components/Test/icon.tsx
@@ -1,23 +1,17 @@
-import { FC, SVGAttributes } from "react";
+import React from "react";
 import * as icons from "@/components/Common/Icon/Icons.def";
 
-interface iIconProps extends SVGAttributes<SVGElement> {
-    icon: keyof typeof icons;
-    width: number;
-    height?: number;
-    fill?: string;
-}
-
-const Icon: FC<iIconProps> = ({ icon, width = 24, height = 24, fill = "none", ...props }) => {
-    const SVGIcon = icons[icon]; // Resolve the icon dynamically
-
-    if (!SVGIcon) {
-        console.error(`Icon "${icon}" does not exist in the icons object.`);
-        return (<></>);
-    }
-    // @ts-ignore
-    return <SVGIcon width={width} height={height} fill={fill} {...props} />;
+const AllIcons = () => {
+    return (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "20px", padding: "20px" }}>
+            {Object.entries(icons).map(([iconName, IconComponent]) => (
+                <div key={iconName} style={{ textAlign: "center" }}>
+                    <IconComponent width={50} height={50} fill="none" />
+                    <p style={{ marginTop: "8px", fontSize: "14px" }}>{iconName}</p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
-
-export default Icon;
+export default AllIcons;
--- src/routes/RoutesGroup.tsx
+++ src/routes/RoutesGroup.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import Icon from "@/components/Test/icon";
 const RoutesGroup = () => {
 
     return (
         <Routes>
             <Route path={"/"} element={<Home />}/>
             <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/test/icons"} element={<Icon/>}/>
 
         </Routes>
     );
