bd862222817123a2355a448c369af275f22f2341
heath <jannhyunsoo@gmail.com>
2025-01-30 23:28:34 +0900

    feat: 개인정보페이지

---
+++ src/pages/Info/subpages/MyInfo/MyInfo.styled.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+
+export const MyInfoWrapper = styled.div`
+    padding: 0.8rem;
+    height: 100%;
+`;
+
+
+export const MyInfoContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    Button {
+        width: 100%;
+        border-radius: 8px;
+    }
+`;
+
+export const NicknameSect = styled.div`
+`;
+
+
+export const CellphoneSect = styled.div`
+
+
+`;
+
+export const EmailSect = styled.div`
+    flex: 1;
+
+`;
+
+
+export const FooterSect = styled.div`
+    flex: 0;
+
+    Button {
+        width: 100%;
+        border-radius: 8px;
+    }
+`;
+
+
+export const InputBox = styled.input`
+    margin: 10px 0;
+    width: 100%;
+    border: 0.5px solid lightgray;
+    height: 46px;
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.colors.blueGrey05};
+
+    // 글자
+    font-size: 16px;
+    font-weight: ${({ theme }) => theme.fontWeights.medium};
+    line-height: 130%;
+    padding: 10px;
+`;
+
+export const TitleBox = styled.p`
+    font-size: ${({ theme }) => theme.fontSizes.sm};
+    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+    color: ${({ theme }) => theme.colors.blueGrey90};
+    line-height: 150%;
+`;
--- src/pages/Info/subpages/MyInfo/index.tsx
+++ src/pages/Info/subpages/MyInfo/index.tsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from "react";
+import { setHeader } from "@/store/slices/uiSlice";
+import { useDispatch } from "react-redux";
+import CustomButton from "@/components/Button";
+import {
+    MyInfoContainer, MyInfoWrapper, NicknameSect,
+    InputBox,
+    TitleBox,
+    EmailSect, FooterSect, CellphoneSect
+} from "@/pages/Info/subpages/MyInfo/MyInfo.styled";
+
 const MyInfo = () => {
+    const [isReady, setReady] = useState(false);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setHeader("개인 정보 수정")); // 헤더 확인
+        setReady(true); // 화면 준비
+    }, []);
+
+
     return (
-        <>
-            MyInfo
-        </>
-    )
-}
+        <MyInfoWrapper>
+            <MyInfoContainer>
+
+                <NicknameSect>
+                    <InputBox></InputBox>
+                </NicknameSect>
+
+
+                <CellphoneSect>
+                    <TitleBox>휴대폰 번호</TitleBox>
+                    <InputBox/>
+                </CellphoneSect>
+
+
+                <EmailSect>
+                    <TitleBox>이메일 주소</TitleBox>
+                    <InputBox/>
+                </EmailSect>
+
+
+                <FooterSect>
+                    <CustomButton type="button" color="primary" size="large" onClick={() => {
+                    }}>
+                        저장하기
+                    </CustomButton>
+                </FooterSect>
+            </MyInfoContainer>
+        </MyInfoWrapper>
+    );
+};
+
 
 export default MyInfo;
