560ed7054f80b9216f5c71a8fb5098225a37cb04
heath <jannhyunsoo@gmail.com>
2025-02-19 23:57:46 +0900

    feat: 개인정보 수정

--- src/pages/Info/subpages/MyInfo/index.tsx
+++ src/pages/Info/subpages/MyInfo/index.tsx
@@ -10,45 +10,81 @@ import {
 } from "@/pages/Info/subpages/MyInfo/MyInfo.styled";
 
 const MyInfo = () => {
-    const [isReady, setReady] = useState(false);
     const dispatch = useDispatch();
+    const [formData, setFormData] = useState({
+        nickname: "",
+        buddyName: "",
+        phoneNumber: "",
+        emailAddress: ""
+    });
 
     useEffect(() => {
-        dispatch(setHeader("개인 정보 수정")); // 헤더 확인
-        setReady(true); // 화면 준비
-    }, []);
+        dispatch(setHeader("개인 정보 수정")); // 헤더 설정
+    }, [dispatch]);
 
+    // 입력값 변경 핸들러
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value // 기존 값 복사 후, 변경된 값 업데이트
+        }));
+    };
+
+    const handleConfirm = () => {
+        console.log("저장된 데이터:", formData);
+        // 저장 로직 추가 가능
+    };
 
     return (
         <MyInfoWrapper>
             <MyInfoContainer>
                 <NicknameSect>
                     <TitleBox>닉네임</TitleBox>
-
-                    <InputBox></InputBox>
+                    <InputBox
+                        name="nickname"
+                        value={formData.nickname}
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="닉네임을 입력하세요"
+                    />
                 </NicknameSect>
-
 
                 <NicknameSect>
                     <TitleBox>버디</TitleBox>
-                    <InputBox/>
+                    <InputBox
+                        name="buddyName"
+                        value={formData.buddyName}
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="버디 이름을 입력하세요"
+                    />
                 </NicknameSect>
 
                 <CellphoneSect>
                     <TitleBox>휴대폰 번호</TitleBox>
-                    <InputBox/>
+                    <InputBox
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        type="tel"
+                        placeholder="휴대폰 번호를 입력하세요"
+                    />
                 </CellphoneSect>
-
 
                 <EmailSect>
                     <TitleBox>이메일 주소</TitleBox>
-                    <InputBox/>
+                    <InputBox
+                        name="emailAddress"
+                        value={formData.emailAddress}
+                        onChange={handleChange}
+                        type="email"
+                        placeholder="이메일을 입력하세요"
+                    />
                 </EmailSect>
 
-
                 <FooterSect>
-                    <CustomButton type="button" color="primary" size="large" onClick={() => {
-                    }}>
+                    <CustomButton type="button" color="primary" size="large" onClick={handleConfirm}>
                         저장하기
                     </CustomButton>
                 </FooterSect>
@@ -56,6 +92,5 @@ const MyInfo = () => {
         </MyInfoWrapper>
     );
 };
-
 
 export default MyInfo;
